09097e6a4b8cf25897965b584b7a440595a47d9d
KOREAN139 <korean139@gmail.com>
2016-07-06 19:14:23 +0000

    Navigation & basic posting feature added, need to fix the error which occur when 'post/' doesn't exists

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -22,6 +22,10 @@ app.config["MAIL_PASSWORD"] = MAIL_PASSWORD
 from flask.ext.mail import Mail, Message
 mail = Mail(app)
 from decorators import async
+import datetime # for get the current time
+import os # for check whether the path exists
+import commands # for create directory
+import glob # for get filenames
 
 @async
 def send_async_email(app, msg):
@@ -57,6 +61,88 @@ def contact_mail():
 					u'반갑습니다 바디입니다',
 					u'%s님께서 보내신 건의는 정상적으로 접수되었습니다. 더욱 노력하는 학생회가 되겠습니다. 감사합니다.'%(name))
 		return redirect(url_for('lend'))
+
+"""
+: blog util function
+"""
+
+def createDirectory(directoryName):
+	if not os.path.exists(directoryName):
+		command = 'mkdir %s'%directoryName
+		ret = commands.getoutput(command)
+		command = 'chmod 777 %s'%directoryName
+		ret = commands.getoutput(command)
+
+def get_max_post_number(filenames):
+	ret = 0
+	for filename in filenames:
+		only_filename = filename.split('/')[-1]
+		only_number = int(only_filename.split('.')[0])
+		ret = max(ret, only_number)
+	return ret
+
+def get_all_posts():
+	createDirectory('/var/www/flask_blog/flask_blog/post/')
+	filenames = glob.glob('/var/www/flask_blog/flask_blog/post/*.post')
+	return filenames
+
+def get_all_post_information(post_names):
+	ret = []
+	for post_name in post_names:
+		d = {}
+		with open(post_name, 'r') as fp:
+			lines = fp.read().strip().split('\n')
+		d['post_id'] = post_name.split('/')[-1].split('.')[0]
+		d['post_name'] = lines[0]
+		d['post_author'] = lines[1]
+		d['post_date'] = lines[2]
+		d['post_body'] = ''
+		for body in lines[3:]:
+			d['post_body'] += body
+		ret.append(d)
+	return ret
+
+"""
+: blog route
+"""
+
+@app.route('/')
+def main():
+	posts = get_all_posts()
+	ret_posts = get_all_post_information(posts)
+	return render_template('blog.html',ret_posts=ret_posts)
+
+@app.route('/create_post')
+def create_post():
+	posts = get_all_posts()
+	ret_posts = get_all_post_information(posts)
+	return render_template('create_post.html',ret_posts=ret_posts)
+
+@app.route('/save_post',methods=['POST'])
+def save_post():
+	if request.method == 'POST':
+		post_name = request.form['post_name']
+		post_author = request.form['post_author']
+		post_body = request.form['post_body']
+		post_date = datetime.datetime.now()
+		s = '%s\n%s\n%s\n%s'%(post_name,post_author,str(post_date),post_body)
+		print post_name, post_author, post_body, post_date
+		post_names = get_all_posts()
+		last_post_number = get_max_post_number(post_names)
+		next_filename = 'var/www/flask_blog/flask_blog/post/%d.post'%(last_post_number+1)
+		with open(next_filename, 'w') as fp:
+			fp.write(s)
+		return redirect(url_for('main'))
+
+@app.route('/post/<post_id>')
+def post_detail(post_id):
+	posts = get_all_posts()
+	ret_posts = get_all_post_information(posts)
+	ret = {}
+	for post in ret_posts:
+		if post['post_id'].strip() == post_id.strip():
+			ret = post
+	return render_template('post.html',ret_posts=ret_posts,post=ret)
 
 if __name__ == '__main__':
 	app.run(host='172.31.14.58', debug=True)
